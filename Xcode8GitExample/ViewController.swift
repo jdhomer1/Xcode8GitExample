89a2bfb258ac2b8dcf117daa3c459ca6d9b30915
James H. Davis <jameshdavis@james-mbp.zoomtown.com>
2017-09-03 22:12:40 -0400

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by James H. Davis on 9/3/17.
+//  Copyright © 2017 com.James H. Davis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
